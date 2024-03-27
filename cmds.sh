4b9c7ca8def8c3d452da6606b5e128fc3da38b14
nmabe <nmabe6212@gmail.com>
2024-03-27 16:11:27 +0200

    add some install setup helpers

---
+++ cmds.sh
@@ -0,0 +1,276 @@
+[200~sudo dpkg --add-architecture i386
+sudo apt update
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install libnvidia-gl-535:i386~
+sudo apt install libnvidia-gl-535:i386
+sudo apt install '/home/nick/Downloads/google-chrome-stable_current_amd64.deb' 
+git --version
+sudo apt install git
+ls
+pwd
+mkdir repos
+git --version
+cd repos
+git clone https://github.com/winter5un/c-_template.git
+ssh-keygen --help
+ssh-keygen -t rsa -b 4096 -C "nmabe6212@gmail.com"
+ssh-add /home/nick/.ssh/github_id_rsa
+cat /home/nick/.ssh/github_id_rsa.pub
+ssh -T git@github.com
+git clone https://github.com/winter5un/c-_template.git
+git clone git@github.com:winter5un/c-_template.git
+ls
+clear
+mkdir godot
+cd godot/
+git clone https://github.com/winter5un/ThinManaLine.git
+git config --global user.email "nmabe6212@gmail.com"
+git config --global user.name "nmabe"
+git clone https://github.com/winter5un/ThinManaLine.git
+git clone git@github.com:winter5un/ThinManaLine.git
+cd ..
+git clone git@github.com:winter5un/vscode_python_template.git
+git clone git@github.com:winter5un/codenoates.git
+git clone git@github.com:winter5un/codenotes.git
+clear
+ls
+cp c-_template/ cpp_study_project
+cp c-_template/ cpp_study_project -r
+ls
+cd cpp_study_project/
+ls -r
+ls src/ -r
+sudo apt install vim
+vscode .
+code .
+ls
+pwd
+cat /etc/systemd/logind.conf
+gnome-tweaks
+sudo apt install gnome-tweaks
+gnome-tweaks
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install chrome-gnome-shell
+./nrf_sniffer_ble.sh --extcap-interfaces
+python3 --version
+python3 pip install requirements.txt 
+python3 -m pip install requirements.txt 
+python3 -m pip install -r requirements.txt 
+pip install -r requirements.txt 
+sudo apt install python3-pip
+pip install -r requirements.txt 
+python 3 -m pip
+python3 -m pip
+python3 -m pip install -r requirements.txt 
+./nrf_sniffer_ble.sh --extcap-interfaces
+python3 -m pip install -r requirements.txt --break-system-packages
+./nrf_sniffer_ble.sh --extcap-interfaces
+sudo apt install wireshark
+./nrf_sniffer_ble.sh 
+./nrf_sniffer_ble.sh --extcap-interfaces
+git clone git@github.com:winter5un/weather_station.git
+cmake --version
+where cmake
+which cmake
+sudo apt install cmake
+/home/nick/repos/cpp_study_project/build/src/MyApp
+poetry shell
+sudo apt install python3-poetry
+poetry install
+touch README.md
+poetry install
+poetry shell
+. /home/nick/.cache/pypoetry/virtualenvs/default-project-yM-H90DH-py3.11/bin/activate
+poetry add tabulate
+poetry add types-tabulate
+python src/open_weather/construction.py 
+python src/open_weather/construction.py tampere
+ls
+cd src
+ls
+pwd
+tmux
+sudo apt install tmux
+sudo apt install brave
+snap install brave
+sudo apt-get update
+sudo apt-get upgrade
+user
+$user
+whoami
+sudo visudo
+sudo apt-get update
+touch ubuntu_notes.md
+code .
+tar xzvf nvim-linux64.tar.gz
+./nvim-linux64/bin/nvim
+nvim
+ls
+cd repos/weather_station/
+ls
+cd src/open_weather/
+ls
+clera
+clear
+nevim
+nvim
+sudo apt uninstall vim
+sudo apt remove vim
+sudo apt install neovim
+nvim
+vim
+clear
+nvim --version
+sudo apt install python3-neovim
+sudo apt list neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+sudo apt remove neovim
+nvim
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+sudo apt install curl
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+sudo apt-get install python3-vev
+sudo apt-get install python3-venv
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+sudo apt-get install python3-pynvim
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+nvim
+lvim
+pwd
+ls .local/
+ls .local
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+rustup default stable
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+lvim
+ls .local
+ls .local/bin/
+./.local/bin/lvim 
+./.local/bin/lvim
+vim .local/bin/lvim 
+nvim .local/bin/lvim 
+./.local/bin/lvim
+cd repos/weather_station/src/open_weather/
+clera
+clear
+sudo apt install unzip
+bash ~/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
+lvim
+clear
+nvim
+sudo apt install ripgrep
+clera
+clear
+git clone https://github.com/winter5un/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+cd ~
+ls .config/
+cd .config/nvim/
+ls
+rm init.vim 
+git clone https://github.com/winter5un/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+ls
+ls lua/
+ls -R
+cat doc/kickstart.txt | more
+clear
+cd ~
+tmux
+cd repos/weather_station/
+nvim
+nvim src/open_weather/construction.py 
+exit
+cd .config/nvim/
+ls
+nvim init.lua 
+exit
+fc-cache -f -v
+clear
+ls
+exit
+ls
+poetry --version
+poetry config --list
+poetry config virtualenvs.in-project true
+poetry config --list
+cd repos/weather_station/
+poetry install
+ls
+ls -a
+poetry env list
+poetry env list --full-path
+poetry env remove defalt
+poetry env remove ~/.cache/pypoetry/virtualenvs/default-project-yM-H90DH-py3.11/
+sudo poetry env remove ~/.cache/pypoetry/virtualenvs/default-project-yM-H90DH-py3.11/
+deactivate
+poetry env remove ~/.cache/pypoetry/virtualenvs/
+rm -rf ~/.cache/pypoetry/virtualenvs/default-project-yM-H90DH-py3.11/
+poetry install
+ls
+ls -a
+poetry shell
+. /home/nick/repos/weather_station/.venv/bin/activate
+dea ctivate
+deactivate
+poetry env remove
+poetry env remove .venv/
+poetry env remove -f .venv/
+poetry env remove --all
+clear
+poetry install
+ls
+ls -a
+ls .venv/
+cat .venv/pyvenv.cfg 
+git clone https://github.com/github/copilot.vim.git   ~/.config/nvim/pack/github/start/copilot.vim
+exit
+sudo apt-get install gtop
+sudo apt-get list gtop*
+sudo apt list gtop*
+sudo apt list GTop*
+sudo apt list *GTop*
+sudo apt-get install libgtop
+clear
+tmux ls
+tmux
+tmux ls
+tmux -a
+tmux -a 0
+tmux --a 0
+tmux -h
+tmux --h
+tmux --help
+man tmux
+tmux attach -t 0
+clear
+tmux
+shutdown
+exit
+pwd
+cd /usr/share/
+ls
+clear
+ls
+ls fonts
+cp ~/Downloads/Hack/ fonts -r
+sudo cp ~/Downloads/Hack/ fonts -r
+ls fonts
+ls fonts/Hack/
+clear
+nvim .config/nvim/init.lua 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+exit
+lazygit
+exit
+lazygit
+lazygit
+cd repos/weather_station/
+lazygit
+cd  ~
+exit
