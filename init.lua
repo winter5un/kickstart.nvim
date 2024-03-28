f324849553fb39d74bb6b82fa1cae4f4bfc0edd0
nmabe <nmabe6212@gmail.com>
2024-03-28 08:56:22 +0200

    change install commands location

--- init.lua
+++ init.lua
@@ -162,9 +162,8 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- my custom options
--- vim.opt.autowriteall = true
---testthis
 --
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
